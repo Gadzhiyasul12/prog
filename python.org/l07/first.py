0143ae6f3a880e2bbd8acff543c9d8b91bca44dc
Gadzhiyasul12 <32506985+Gadzhiyasul12@users.noreply.github.com>
2017-10-18 11:36:04 +0300

    Add files via upload

---
+++ python.org/l07/first.py
@@ -0,0 +1,8 @@
+import random
+def my_class():
+	r = random.randint(1, 4)
+	if r == 1 or == 2:
+    	print("Доброе утро!")
+    elif r == 3 or == 4:
+    	print("Не доброе утро")
+my_class()
---
+++ python.org/l07/first3.py
@@ -0,0 +1,7 @@
+def lavash_factorial(number):
+	i = 1
+	result = 1
+	while i <= number:
+		result *= 1
+		i += 1
+	return result
