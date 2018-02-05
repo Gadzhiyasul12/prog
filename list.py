d95353d912ade616231e9462e87288f3823637ec
Gadzhiyasul12 <32506985+Gadzhiyasul12@users.noreply.github.com>
2018-02-05 09:48:23 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+my_list = ["У меня тянет GTA 5 на минималках в 25-60FPS"]
+print(my_list)
+
+chaos_list = [1, "Строка", True,None]
+print(chaos_list)
+
+print(my_list[1:3])
+my_list[0] = "у меня тянет Watch Dogs 2 на максималках в 100-150FPS"
+print(my_list)
+my_list.append("Мемы для Исмаила Ахмедова")
+my_list.insert(2,"Мемы для аутистов")
+my_list.extend("Исма в тумане")
+print(my_list)
+my_list.remove("25-60FPS")
+del my_list[3]
+print(my_list)
+list_of_numb = [1,4,2,]
---
+++ repit.py
@@ -0,0 +1,9 @@
+bob = "Кто проживает на дне океана"
+print(bob)
+print(bob[6:9])
+print(bob[-6:])
+print(bob[:5])
+print(bob[::2])
+print(len(bob))
+print(bob.replace("К","к")
+print(bob)
